5e2747450c34922bb73bf0e94b5ae64ef4732866
青木 <qingmu.io@qq.com>
2021-03-05 10:16:19 +0800

    new

--- hadoop/Build.sh
+++ hadoop/Build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker login --username=247687009@qq.com -p QT0jRZFlJ5sAzytgwRONuV599mhBMj registry.cn-shanghai.aliyuncs.com
 IMAGE_BASE_URL=registry.cn-shanghai.aliyuncs.com/qingmuio IMAGE_TAG=3.2.2 make
